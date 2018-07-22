778795bb70abe21ed90a3cd6670e00958fa28b38
Charlie Russo <cparusso@gmail.com>
2018-07-22 19:09:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,6 +42,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+
+}
+
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
 
